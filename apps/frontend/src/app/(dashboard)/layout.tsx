fe7d4fa12c022b571e005809dd1edde5f0184fdf
hank <hankchiu.tw@gmail.com>
2025-10-01 23:05:52 +0800

    fix(dashboard): update 'Get Help' link to new GitHub repo
    - Changed the help link from old repository to 'https://github.com/coxy-proxy/coxy/issues'
    - Ensures users are redirected to the correct support location

--- apps/frontend/src/app/(dashboard)/layout.tsx
+++ apps/frontend/src/app/(dashboard)/layout.tsx
@@ -69,7 +69,7 @@ function UserMenuItem() {
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href="https://github.com/hankchiutw/copilot-proxy/issues" target="_blank">
+          <Link href="https://github.com/coxy-proxy/coxy/issues" target="_blank">
             Get Help
           </Link>
         </DropdownMenuItem>
